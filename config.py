a2a63278d5d5d621ead3e7ce9393ee97555366c4
DEENAJA BATCHU <159751906+deenajabatchu@users.noreply.github.com>
2025-06-10 20:40:25 +0530

    config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Deenu_512@localhost/notes_db'
+SQLALCHEMY_DATABASE_URI =  'mysql+mysqlconnector://root:your_password@localhost/notes_db'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_ECHO = True
